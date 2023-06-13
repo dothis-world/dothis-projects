6a1c3871a614a36423882e4717ab8744d4d5fc10
mini <jma1020@naver.com>
2023-06-13 23:33:52 +0900

    test: 임시 verify token api 생성

--- apps/dashboard-web/src/api/verify.ts
+++ apps/dashboard-web/src/api/verify.ts
@@ -1,15 +1,22 @@
 import instance from './intance';
 
-export const verifyToken = async () => {
+export const verifyToken = async ({
+  cookie,
+}: {
+  cookie: string | undefined;
+}) => {
   try {
-    const data = await instance.get('/');
+    console.log(cookie);
+    const data = await instance.get('/v1/auth/verify-token', {
+      headers: {
+        Authorization: cookie,
+      },
+    });
 
-    console.log(data);
-    return {
-      data,
-    };
+    return data;
   } catch (err) {
-    console.log('에러 발결 ' + err);
+    console.log('에러 발결 ');
+    console.log(err);
     return err;
   }
 };
