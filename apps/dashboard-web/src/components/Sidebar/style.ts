6a8ced73175a1545c9cbad3241d12260353a33dc
mimi <62527898+HM970506@users.noreply.github.com>
2023-06-19 15:31:03 +0900

    chore: css to ts

---
+++ apps/dashboard-web/src/components/Sidebar/style.ts
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const SidebarLayout = styled.nav`
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  flex-direction: column;
+
+  height: 3000px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  z-index: 1001;
+`;
+
+export const Logo = styled.div`
+  width: 50px;
+  height: 50px;
+  margin: 24px;
+  margin-bottom: 60px;
+
+  border: 1px solid black;
+`;
+
+export const NavButtonsContainer = styled.div`
+  display: flex;
+  justify-content: left;
+  align-items: center;
+  flex-direction: column;
+
+  gap: 40px;
+  padding: 0px 24px;
+  button {
+    width: 50px;
+    height: 50px;
+    border-radius: 8px;
+    background-color: inherit;
+    border: 1px solid #e4e4e7;
+  }
+`;
