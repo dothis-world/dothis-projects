7f366c6e2a351d92fa6bb3b7917c18fbfe208a9e
mini <jma1020@naver.com>
2023-06-20 21:47:57 +0900

    Chore: SideBar Svg 파일 추가 및 export

--- apps/dashboard-web/src/assets/index.ts
+++ apps/dashboard-web/src/assets/index.ts
@@ -1,2 +1,8 @@
 export { default as LogoSvg } from './dothisLogo.svg';
 export { default as GoogleSvg } from './Google.svg';
+export { default as SideLogo } from './SideLogo.svg';
+export { default as SideMagicPen } from './SideMagicPen.svg';
+export { default as SideMain } from './SideMain.svg';
+export { default as SideMessage } from './SideMessage.svg';
+export { default as SideTrendUp } from './SideTrendUp.svg';
+export { default as SideUser } from './SideUser.svg';
