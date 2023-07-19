3da153ff15f5cc62ebb8e3592bdbf94cfd83b372
mini <jma1020@naver.com>
2023-07-19 11:36:40 +0900

    docs: 주석추가

--- apps/dashboard-web/src/constants/dev.ts
+++ apps/dashboard-web/src/constants/dev.ts
@@ -1,5 +1,7 @@
 export const serverApiBaseUrl = 'https://api.dothis.kr/v1' as const;
 
+// http://localhost:8080
+// 'https://api.dothis.kr/v1'
 export const mockApiHost = 'http://localhost:3667' as const;
 export const mockApiPathname = '/api/mock' as const;
 export const mockApiBaseUrl = `${mockApiHost}${mockApiPathname}` as const;
