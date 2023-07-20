f7019b217f84a49b90024774f7d436dcfc021693
mini <jma1020@naver.com>
2023-07-20 16:49:14 +0900

    feat: styled theme 파일 생성

---
+++ apps/dashboard-web/src/styles/theme.ts
@@ -0,0 +1,10 @@
+import { colors } from '@dothis/theme/dashboard/colors';
+import type { DefaultTheme } from 'styled-components';
+
+const theme: DefaultTheme = {
+  colors: {
+    ...colors,
+  },
+};
+
+export default theme;
