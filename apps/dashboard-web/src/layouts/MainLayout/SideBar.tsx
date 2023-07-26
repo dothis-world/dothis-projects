271a09819c0347826f1db42aa277a7ca60578c08
mini <jma1020@naver.com>
2023-07-26 13:20:19 +0900

    feat: css inactive 상태추가

--- apps/dashboard-web/src/layouts/MainLayout/SideBar.tsx
+++ apps/dashboard-web/src/layouts/MainLayout/SideBar.tsx
@@ -1,5 +1,10 @@
 'use client';
 
+import './svg.css';
+
+import { usePathname } from 'next/navigation';
+import { css } from 'styled-components';
+
 import { SIDE_MENUS } from '@/constants/SideMenus';
 import SvgComp from '@/share/SvgComp';
 
@@ -9,12 +14,14 @@ import * as Style from './style';
 // transition은 추후에 변경 생각 중
 
 const SideBar = () => {
+  const pathName = usePathname();
+
   return (
     <Style.Container>
       <SvgComp icon="SideLogo" size={50} />
       <div className="flex flex-col gap-[5rem]">
         {SIDE_MENUS.map((item) => (
-          <Style.IconWrapper>
+          <Style.IconWrapper $isInActive={pathName !== item.link}>
             <Style.IconBox>
               <SvgComp icon={item.icon} size={24} />
             </Style.IconBox>
