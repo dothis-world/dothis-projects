55914ff74aa1c30e42166cbb5803f7ffe2b8ea87
mini <jma1020@naver.com>
2023-08-03 15:03:07 +0900

    rename: storybook 파일 생겅

---
+++ packages/dashboard-storybook/src/components/Button/Button.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from './Button';
+
+const meta = {
+  title: 'Components/Button',
+  component: Button,
+  tags: ['autodocs'],
+  argTypes: {
+    label: {
+      defaultValue: '버튼',
+      description: '버튼에 들어갈 텍스트',
+      control: {
+        type: 'text',
+      },
+    },
+    disabled: { control: 'boolean' },
+    size: {
+      option: ['S', 'M', 'L'],
+      control: 'radio',
+    },
+    buttonTheme: {
+      option: ['primary', 'outlined', 'taxted'],
+      control: 'select',
+    },
+  },
+  args: {
+    label: '디폴트',
+    size: 'M',
+    buttonTheme: 'outlined',
+  },
+  // args 부분이 어떤 역할이지?? (default args 느낌인 것 같다.)
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {
+  args: {
+    label: '버튼',
+    size: 'M',
+    buttonTheme: 'outlined',
+  },
+
+  render: (args) => <Button {...args} />,
+  // 다른요소나 태그까지 포함해서 rendering 하고싶을 때 render 사용.
+};
