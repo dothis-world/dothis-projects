52e54e9a61c3efde703c113bbaddfe559529a33f
mini <jma1020@naver.com>
2023-09-25 13:30:00 +0900

    fix: BaseUrl 수정

--- apps/dashboard-web/src/components/Login/GoogleBtn.tsx
+++ apps/dashboard-web/src/components/Login/GoogleBtn.tsx
@@ -1,6 +1,7 @@
 import type { Route } from 'next';
 import Link from 'next/link';
 
+import { apiBaseUrl, BaseURL } from '@/constants/dev';
 import { HTTP_BASE_URL } from '@/constants/http';
 
 import SvgComp from '../../share/SvgComp';
@@ -9,7 +10,7 @@ const GoogleBtn = () => {
   return (
     <Link
       className="mt-[3.75rem] block w-full"
-      href={(HTTP_BASE_URL + '/v1/auth/google-login') as Route}
+      href={(BaseURL + '/v1/auth/google-login') as Route}
     >
       <div className="border-grey200 flex w-full items-center justify-center rounded-lg border border-solid py-4 text-center">
         <button className="inline-flex gap-[0.7rem]">
