e29520086806bfadae54cb0cbc8dfec3fe50d969
godkor200 <godkor200@gmail.com>
2023-09-29 16:54:36 +0900

    refecter : 없음

--- apps/server/apps/api/src/modules/user/database/user.repository.port.ts
+++ apps/server/apps/api/src/modules/user/database/user.repository.port.ts
@@ -3,6 +3,8 @@ import { User } from '../domain/user.entity';
 
 export interface UserRepositoryPort extends RepositoryPort<User> {
   findOneByEmail(userEmail: string): Promise<User>;
+
   updateRefreshToken(id: number, token: string): Promise<void>;
+
   findOneWithRelations(id: string): Promise<User>;
 }
