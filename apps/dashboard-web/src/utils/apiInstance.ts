d5c18fc8e809063efec0cb531a56b9d75128c8c4
mini <jma1020@naver.com>
2023-10-13 08:54:45 +0900

    feat: apiInstance 백엔드 ErrorMessage에 따른 분기처리 추가

--- apps/dashboard-web/src/utils/apiInstance.ts
+++ apps/dashboard-web/src/utils/apiInstance.ts
@@ -10,6 +10,7 @@ import {
   isAccessTokenExpired,
   isRefreshTokenExpired,
   isTokenNotExist,
+  isTokenNotMatch,
 } from './authUtils';
 
 export const HTTP_BASE = '  api.dothis.kr';
@@ -63,6 +64,7 @@ apiInstance.interceptors.response.use(
   async (error) => {
     count += 1;
 
+    console.log(error);
     if (count > 1) {
       count = 0;
       return Promise.reject(error);
@@ -70,6 +72,7 @@ apiInstance.interceptors.response.use(
 
     const { data, config: originalRequest } = error.response;
     const statusCode = data.statusCode;
+    const errorMessage = data.message;
 
     if (isAccessTokenExpired(statusCode)) {
       await apiServer.auth.getVerifyToken();
@@ -88,9 +91,9 @@ apiInstance.interceptors.response.use(
      * 해당 if 절 앞단 true는 백엔드와 Error Text 협의 후 제거 예정
      */
     if (
-      true ||
-      isRefreshTokenExpired(statusCode, 'serverTitle') ||
-      isTokenNotExist(statusCode, 'serverTitle')
+      isRefreshTokenExpired(statusCode, errorMessage) ||
+      isTokenNotExist(statusCode, errorMessage) ||
+      isTokenNotMatch(statusCode, errorMessage)
     ) {
       const { setIsTokenRequired, setIsSignedIn } =
         authStore.getState().actions;
