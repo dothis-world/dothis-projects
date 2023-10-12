50899d249d8e306546eac556340afbf16ff7bd28
mini <jma1020@naver.com>
2023-10-12 20:09:38 +0900

    fix: 의미없는 console 제거 및 리팩토링

--- apps/dashboard-web/src/components/feature/AuthProvider.tsx
+++ apps/dashboard-web/src/components/feature/AuthProvider.tsx
@@ -42,9 +42,8 @@ function AuthProvider({ children }: StrictPropsWithChildren) {
         /**
          * Access Token이 존재하지 않을 경우 재발급
          */
-        const data = await apiServer.auth.getVerifyToken();
+        await apiServer.auth.getVerifyToken();
 
-        console.log(data);
         setIsSignedIn(true);
       } catch (error) {
         if (isAxiosError(error)) {
@@ -52,8 +51,6 @@ function AuthProvider({ children }: StrictPropsWithChildren) {
           const statusCode = err.response?.status as number;
           const errorData = err.response?.data.data;
           const title = errorData?.title as string;
-
-          console.log(err);
 
           /**
            * Refresh Token이 만료되었거나 모든 토큰이 존재하지 않을 경우 홈으로 리다이렉트
