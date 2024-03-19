77a01d8f660b372eabce2e09fb501848d666e4c9
mini <jma1020@naver.com>
2024-03-19 13:54:20 +0900

    feat: SeriesDetail 타입 추가

--- apps/dashboard-web/src/types/apex.d.ts
+++ apps/dashboard-web/src/types/apex.d.ts
@@ -16,3 +16,15 @@ type ChartType =
   | 'rangeBar'
   | 'treemap'
   | undefined;
+
+type SeriesDetail = Extract<
+  ApexAxisChartSeries[number]['data'][number],
+  { x: any; y: any }
+>;
+
+// SeriesDetail 타입의 data 프로퍼티만 변경
+type UpdatedApexAxisChartSeries = {
+  [K in keyof ApexAxisChartSeries]: Omit<ApexAxisChartSeries[K], 'data'> & {
+    data: SeriesDetail;
+  };
+};
