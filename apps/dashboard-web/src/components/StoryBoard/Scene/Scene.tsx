1212bff34176c2c265f1ca9bd711b6378255a142
chaejin-jen <jen71324@gmail.com>
2024-04-17 08:38:30 +0900

    fix: SceneListItem이 Draggable의 자식으로 오도록 수정

--- apps/dashboard-web/src/components/StoryBoard/Scene/Scene.tsx
+++ apps/dashboard-web/src/components/StoryBoard/Scene/Scene.tsx
@@ -4,9 +4,18 @@ import Draggable from '@/components/common/Dnd/Draggable';
 import DraggableContextProvider from '@/components/common/Dnd/DraggableContext';
 
 import SceneControls from './SceneControls';
-import SceneList from './SceneList';
+import SceneListItem from './SceneListItem';
 
 const Scene = () => {
+  // react query data
+  const exData = {
+    1: { description: '설명1', video: '비디오1', audio: '오디오1' },
+    2: { description: '설명2', video: '비디오2', audio: '오디오2' },
+    3: { description: '설명3', video: '비디오3', audio: '오디오3' },
+    4: { description: '설명4', video: '비디오4', audio: '오디오4' },
+    5: { description: '설명5', video: '비디오5', audio: '오디오5' },
+  };
+
   const [isEditing, setIsEditing] = useState<boolean>(false);
   const toggleEdit = () => {
     setIsEditing((prevState) => !prevState);
@@ -40,11 +49,18 @@ const Scene = () => {
             <div className="bg-grey100 grow p-2">Audio</div>
           </div>
           <Draggable className="px-[10px]" handle>
-            <SceneList
-              isEditing={isEditing}
-              checkedItems={checkedItems}
-              toggleChecked={toggleChecked}
-            />
+            {exData &&
+              Object.entries(exData).map(([k, v], i) => (
+                <SceneListItem
+                  sceneNumber={i + 1}
+                  sceneId={k}
+                  defaultValues={v}
+                  key={k}
+                  isEditing={isEditing}
+                  checkedItems={checkedItems}
+                  toggleChecked={toggleChecked}
+                />
+              ))}
           </Draggable>
         </div>
       </div>
