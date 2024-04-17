8dfad66697f3c8a5077655149cc1fbe6652bdfed
mini <jma1020@naver.com>
2024-04-17 15:57:11 +0900

    refactor: StickyContainer 리팩토링

--- apps/dashboard-web/src/components/common/StickyContainer/StickyContainerContext.tsx
+++ apps/dashboard-web/src/components/common/StickyContainer/StickyContainerContext.tsx
@@ -1,15 +1,25 @@
 'use client';
 
 import clsx from 'clsx';
-import React, { createContext, useContext, useRef, useState } from 'react';
+import React, {
+  createContext,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import styled from 'styled-components';
 
-import useIntersectionObserver from '@/hooks/useIntersectionObserver';
+import useDebounce from '@/hooks/useDebounce';
 
 interface StickyContainerState {
   portalId: string;
-  sticky: boolean;
   triggerRef: React.RefObject<HTMLDivElement>;
   stickyDivRef: React.RefObject<HTMLDivElement>;
+  rootMargin: any[];
+  setRootMargin: React.Dispatch<React.SetStateAction<any[]>>;
+  renderTrigger: boolean;
+  setRenderTrigger: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 const StickyContainerContext = createContext<StickyContainerState | null>(null);
@@ -36,23 +46,89 @@ export const StickyContainerContextProvider = ({
   portalId = 'sticky-container-portal',
 }: StickyContainerContextProviderProps) => {
   const triggerRef = useRef<HTMLDivElement>(null);
+
   const stickyDivRef = useRef<HTMLDivElement>(null);
-  const [sticky, setSticky] = useState<boolean>(false);
-  // const triggerRef = useIntersectionObserver(() => {
-  //   setSticky(true);
-  // });
+
+  /**
+   * @rootMargin - rootMargin stickyOrder에 따른 Element들의 정보를 가지고있을 state 생성
+   */
+  const [rootMargin, setRootMargin] = useState<any[]>([]);
+
+  /**
+   * @renderTrigger - context에서 sticky변화에 따라서 render를 제어하는 state가 없어서 renderTrigger용으로 생성
+   * StickyContainer에서 useEffect 디펜던시에 주입할 요소가 필요했음
+   */
+  const [renderTrigger, setRenderTrigger] = useState(false);
+
+  const divRef = useRef<HTMLDivElement | null>(null);
+
+  const [width, setWidth] = useState(
+    divRef.current?.getBoundingClientRect().width,
+  );
+
+  const handleResize = useDebounce(() => {
+    const boundary = divRef.current?.getBoundingClientRect();
+
+    if (boundary) {
+      setWidth(boundary.width);
+    }
+  }, 10); // 300ms 딜레이를 가진 debounce 함수
+
+  // useEffect를 사용하여 컴포넌트가 마운트되면 resize 이벤트를 추가하고, 언마운트 시 이벤트를 제거합니다.
+  React.useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [handleResize, divRef.current]);
 
   return (
     <StickyContainerContext.Provider
-      value={{ portalId, sticky, triggerRef, stickyDivRef }}
+      value={{
+        portalId,
+        triggerRef,
+        stickyDivRef,
+        rootMargin,
+        setRootMargin,
+        renderTrigger,
+        setRenderTrigger,
+      }}
     >
-      {/* <div ref={triggerRef}></div> */}
       <div
-        className={clsx('sticky top-0', className)}
-        ref={stickyDivRef}
-        id={portalId}
-      ></div>
-      {children}
+        className="relative flex w-full flex-col "
+        ref={(node) => {
+          divRef.current = node;
+          if (node) {
+            setWidth(node?.getBoundingClientRect().width);
+          }
+        }}
+      >
+        <PortalDiv
+          ref={stickyDivRef}
+          id={portalId}
+          $isSticky={!!stickyDivRef.current?.children.length}
+          $width={width}
+        ></PortalDiv>
+        {/* classname을 넘겨주는데, 해당 classnaame을 병합하는 코드 추가필요 */}
+        {children}
+      </div>
     </StickyContainerContext.Provider>
   );
 };
+
+const PortalDiv = styled.div<{
+  $width: number | undefined;
+  $isSticky: boolean;
+}>`
+  position: fixed;
+  top: 0;
+
+  width: ${({ $width }) => $width && `${$width}px`};
+  max-width: inherit;
+
+  border: ${({ $isSticky }) => $isSticky && '1px solid black'};
+
+  z-index: 9999;
+
+  background-color: white;
+`;
