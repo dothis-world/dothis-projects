71283a8dacd46335d1002dbcc2f0cb9542967eba
mini <jma1020@naver.com>
2024-05-07 16:33:27 +0900

    remove: 불필요한 타입 제거

--- apps/dashboard-web/src/components/common/Calendar/Calendar.tsx
+++ apps/dashboard-web/src/components/common/Calendar/Calendar.tsx
@@ -1,24 +1,5 @@
 'use client';
 
-type oneToNine = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-type zeroToNine = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-/**
- * Years
- */
-type YYYY = `19${zeroToNine}${zeroToNine}` | `20${zeroToNine}${zeroToNine}`;
-/**
- * Months
- */
-type MM = `0${oneToNine}` | `1${0 | 1 | 2}`;
-/**
- * Days
- */
-type DD = `${0}${oneToNine}` | `${1 | 2}${zeroToNine}` | `3${0 | 1}`;
-/**
- * YYYYMMDD
- */
-type RawDateString = `${YYYY}-${MM}-${DD}`;
-
 import type { Dayjs } from 'dayjs';
 import dayjs from 'dayjs';
 import { useEffect, useState } from 'react';
