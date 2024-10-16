705285f30abdfc608c0df3ed8c0e9091e0e2c520
mini <jma1020@naver.com>
2024-10-16 18:08:13 +0900

    fix : searchBar state 추가로 인한 예외처리 추가

--- apps/dashboard-web/src/app/(keyword)/channel/competitive-analysis/VideoUseTextList.tsx
+++ apps/dashboard-web/src/app/(keyword)/channel/competitive-analysis/VideoUseTextList.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useRef } from 'react';
+
 import { cn } from '@/utils/cn';
 
 import { useVideoUseTextContext } from './VideoUseTextContext';
@@ -7,7 +9,7 @@ const VideoUseTextList = () => {
   const { keywordsCounts, topKeywords } =
     useVideoUseTextContext('VideoUseTextList');
 
-  const { filterKeywords, setFilterKeywords } =
+  const { searchKeyword, filterKeywords, setFilterKeywords, setSearchKeyword } =
     useVideoUseTextFilterContext('VideoUseTextList');
 
   const handleFilterKeyword = (keyword: string) => {
@@ -25,27 +27,74 @@ const VideoUseTextList = () => {
     }
   };
 
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  const handleWheel = (e: WheelEvent) => {
+    if (scrollRef.current) {
+      e.preventDefault();
+      // deltaY 값을 기준으로 가로로 스크롤
+      e.stopPropagation();
+      scrollRef.current.scrollLeft += e.deltaY;
+    }
+  };
+
+  useEffect(() => {
+    const currentRef = scrollRef.current;
+    if (currentRef) {
+      currentRef.addEventListener('wheel', handleWheel, { passive: false });
+    }
+
+    // 초기 스크롤 상태 확인
+
+    // cleanup function
+    return () => {
+      if (currentRef) {
+        currentRef.removeEventListener('wheel', handleWheel);
+      }
+    };
+  }, []);
+
   return (
-    <div className="border-grey400 rounded-10 ml-auto flex items-center  gap-[20px] border px-[20px] py-[8px]">
+    <div className="border-grey400 rounded-10  ml-auto flex items-center overflow-hidden whitespace-nowrap border px-[20px] py-[8px]">
       <p className="text-grey600 mr-[10px] text-[14px] font-[500]">
         주요 키워드
       </p>
 
-      {topKeywords?.map((item) => (
-        <div
-          className={cn(
-            'border-grey500 rounded-8 text-grey600 border px-[20px] py-[8px] font-[400] cursor-pointer transition-all duration-300',
-            {
-              'bg-primary50 text-primary500 border-primary500':
-                filterKeywords?.includes(item),
-            },
-          )}
-          key={item}
-          onClick={() => handleFilterKeyword(item)}
-        >
-          {`${item} (${keywordsCounts?.[item] ?? 0})`}
-        </div>
-      ))}
+      <div
+        className="custom-scroll-x-box  flex items-center gap-[20px] overflow-x-scroll scroll-smooth"
+        ref={scrollRef}
+      >
+        {topKeywords?.map((item) => (
+          <div
+            className={cn(
+              'border-grey500 rounded-8 text-grey600 border px-[20px] py-[8px] font-[400] cursor-pointer transition-all duration-300',
+              {
+                'bg-primary50 text-primary500 border-primary500':
+                  filterKeywords?.includes(item) && !searchKeyword,
+              },
+            )}
+            key={item}
+            onClick={() => handleFilterKeyword(item)}
+          >
+            {`${item} (${keywordsCounts?.[item] ?? 0})`}
+          </div>
+        ))}
+
+        {searchKeyword && (
+          <div
+            className={cn(
+              'border-grey500 rounded-8 text-grey600 border px-[20px] py-[8px] font-[400] cursor-pointer transition-all duration-300',
+              {
+                'bg-primary50 text-primary500 border-primary500': searchKeyword,
+              },
+            )}
+            key={searchKeyword}
+            onClick={() => setSearchKeyword(null)}
+          >
+            {`${searchKeyword}`}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
