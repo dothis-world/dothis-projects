553f9407d8fa94fb9ca74c74fb3aa13e38eb86d9
mini <jma1020@naver.com>
2024-10-23 04:53:21 +0900

    feat : 라우팅 연결

--- apps/dashboard-web/src/app/(keyword)/TrendKeywordList.tsx
+++ apps/dashboard-web/src/app/(keyword)/TrendKeywordList.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import type { Route } from 'next';
+import Link from 'next/link';
+
 import { LandingButton } from '@/components/MainContents/KeywordSearch/style';
 import useGetWeeklyTrendKeyword from '@/hooks/react-query/query/useGetWeeklyTrendKeyword';
 
@@ -9,15 +12,20 @@ const TrendKeywordList = () => {
   return (
     <div className="flex flex-wrap justify-between gap-x-[30px] gap-y-[20px]">
       {data?.map((item, index) => (
-        <LandingButton
+        <Link
+          href={`/keyword/${item.recommendedKeyword}` as Route}
           key={item.recommendedKeyword}
-          $active={index % 2 === 1 ? true : false}
-          className="cursor-pointer"
         >
-          {item.recommendedKeyword}
+          <LandingButton
+            key={item.recommendedKeyword}
+            $active={index % 2 === 1 ? true : false}
+            className="cursor-pointer"
+          >
+            {item.recommendedKeyword}
 
-          {/* 여기서 X버튼으로 delete시 modal을 하나 생성하고 지우는게 좋을 듯 싶다. */}
-        </LandingButton>
+            {/* 여기서 X버튼으로 delete시 modal을 하나 생성하고 지우는게 좋을 듯 싶다. */}
+          </LandingButton>
+        </Link>
       ))}
     </div>
   );
