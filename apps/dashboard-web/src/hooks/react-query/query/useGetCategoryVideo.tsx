2118e2de868e8994a6db277534356399e0b30444
mini <jma1020@naver.com>
2024-10-23 04:47:19 +0900

    feat : useGetCategoryVideo query hook 추가

---
+++ apps/dashboard-web/src/hooks/react-query/query/useGetCategoryVideo.tsx
@@ -0,0 +1,67 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
+import { CATEGORY_VIDEO_LIST_KEY } from '@/constants/querykey';
+
+const reqUrl = 'https://api.dothis.kr/v1/video/issue/category';
+
+interface Video {
+  videoId: string;
+  videoViews: number;
+  videoTitle: string;
+  videoPublished: string;
+  avgViews: string;
+}
+
+interface ApiResponse {
+  success: boolean;
+  data: { data: Video[] };
+}
+
+const queryFn = async ({
+  categoryNumbers,
+  limit,
+  startDate,
+  endDate,
+}: {
+  categoryNumbers: number[];
+  limit: number;
+  startDate: string;
+  endDate: string;
+}): Promise<ApiResponse> => {
+  const formattedCategoryNumbers = categoryNumbers.join(',');
+  return await axios.get(reqUrl, {
+    params: {
+      categoryNumbers: formattedCategoryNumbers,
+      limit,
+      to: endDate,
+      from: startDate,
+    },
+  });
+};
+const useGetCategoryVideo = ({
+  categoryNumbers,
+  limit,
+  startDate,
+  endDate,
+}: {
+  categoryNumbers: number[];
+  limit: number;
+  startDate: string;
+  endDate: string;
+}) => {
+  const queryResult = useQuery<ApiResponse>(
+    CATEGORY_VIDEO_LIST_KEY.list([
+      { categoryNumbers, limit, startDate, endDate },
+    ]),
+
+    () => queryFn({ categoryNumbers, limit, startDate, endDate }),
+    {
+      enabled: !!startDate && !!endDate,
+    },
+  );
+
+  return { ...queryResult };
+};
+
+export default useGetCategoryVideo;
