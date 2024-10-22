dca1a2d61869c3e49c5bcbefd1e319f179dad090
mini <jma1020@naver.com>
2024-10-23 04:46:47 +0900

    fix : props 추가에 따른 queryKey 반영

--- apps/dashboard-web/src/hooks/react-query/query/useGetWeeklyTrendKeyword.ts
+++ apps/dashboard-web/src/hooks/react-query/query/useGetWeeklyTrendKeyword.ts
@@ -3,7 +3,7 @@ import { apiClient } from '@/utils/api/apiClient';
 
 const useGetWeeklyTrendKeyword = ({ limit }: { limit: number }) => {
   const queryResult = apiClient(1).hits.getKeywordThisWeekly.useQuery(
-    WEEKLY_TREND_KEYWORD_KEY.all,
+    WEEKLY_TREND_KEYWORD_KEY.list([{ limit }]),
     {
       query: {
         limit: limit.toString(),
